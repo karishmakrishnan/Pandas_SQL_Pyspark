4a6a15e23c9f2ecd2ea766984d5bca37b1a71266
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-02-18 12:25:37 +0530

    join to DF using left join

---
+++ Leetcode_pandas_merge_df.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    # output = pd.DataFrame()
+    # print(person)
+    output = person.merge(address, on = ["personId"], how="left")
+    # output = output[["firstName", "lastName", "city", "state"]]
+    # print(output)
+    return output[["firstName", "lastName", "city", "state"]]
