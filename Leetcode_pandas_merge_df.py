d9b292708c62a29ed5d89d901da4f1c6e6f082b0
karishmakrishnan <62092690+karishmakrishnan@users.noreply.github.com>
2026-02-19 21:33:22 +0530

    Fix return statement in combine_two_tables function

--- Leetcode_pandas_merge_df.py
+++ Leetcode_pandas_merge_df.py
@@ -7,3 +7,13 @@ def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFr
     # output = output[["firstName", "lastName", "city", "state"]]
     # print(output)
     return output[["firstName", "lastName", "city", "state"]]
+
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    # output = pd.DataFrame()
+    # print(person)
+    output = person.merge(address, on = ["personId"], how="left")
+    # output = output[["firstName", "lastName", "city", "state"]]
+    # print(output)
+    return output[["firstName", "lastName", "city", "state"]]
